b82e908d5d489aa42a03728fb327bdcb7cd12605
Bernardo Alves <bernardoalves330@gmail.com>
2024-09-05 21:46:06 -0300

    adicionando user no db

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -9,12 +9,12 @@ class UserController {
 
     async createUser(req: Request, res: Response): Promise<Response> {
         const { name, email, password } = req.body;
-
+        //TODO: implementar a criptografação de senha, atualmente salvando como plain text
         const newUser = new User({ name, email, password })
 
         try {
             this.userRepository.createUser(newUser)
-            return res.status(201)
+            return res.status(201).json({ id: newUser.id })
         } catch (e) {
             if (e instanceof Error) res.json({ msg: e.message })
             return res.json({ msg: 'unknown error' })
--- backend/src/repositories/implementation/SqlLiteUserRepository.ts
+++ backend/src/repositories/implementation/SqlLiteUserRepository.ts
@@ -6,6 +6,7 @@ export class SqlLiteUserRepository implements IUserRepository {
     async createUser(user: User): Promise<void> {
         await prismaClient.user.create({ data: user })
     }
+    //TODO implementação correta da funcão
     async getUser(id: string): Promise<User | undefined> {
         await prismaClient.user.findUnique({
             where: { id }
